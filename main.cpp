04dede885b52f877c2f41986d6d640c26017c022
Xiaoctw <m18846183092@163.com>
2018-03-19 11:38:14 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for (int i = 0; i <100 ; ++i) {
+        printf("%d ",i*i);
+    }
+    return 0;
+}
